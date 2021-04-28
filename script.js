f62598fb38bd01f0122b444b0ca248ceafa4693e
Steve Mackidd <s.mackidd425@mybvc.ca>
2021-04-27 21:56:48 -0600

    button click completed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 
 function openGlass() {
-  gsap.to(".circle2", {boxShadow: "inset 8px 8px 21px #1f2529, inset -8px -8px 21px #293337"})
-  gsap.to(".glassLeft", {duration: 5, x: "-100%", delay: 1, ease: "bounce"});
-  gsap.to(".glassRight", {duration: 5, x: "100%", delay: 1, ease: "bounce"});  
+  gsap.to(".circle2", {boxShadow: "inset 8px 8px 21px #1f2529, inset -8px -8px 21px #293337"});
+  gsap.to(".blocks", {opacity: 1, delay: 0.5, stagger: 0.05, repeat: 1, yoyo: true})
+  gsap.to(".glassLeft", {duration: 4, x: "-100%", delay: 2, ease: "bounce"});
+  gsap.to(".glassRight", {duration: 4, x: "100%", delay: 2, ease: "bounce"});
+  gsap.to(".circle1, .circle2", {duration: 3, opacity: 0, delay: 4, ease: "slow"});  
 }
