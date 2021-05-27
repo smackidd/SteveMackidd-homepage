e8ebf0d329ee22e6b214c850e21aca8da4f53ade
Steve Mackidd <55397135+smackidd@users.noreply.github.com>
2021-05-27 14:48:19 -0600

    adjusted home section

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ const tlAbout = gsap.timeline({
 
 function openGlass() {
   gsap.to(".circle2", {boxShadow: "inset 8px 8px 21px #1f2529, inset -8px -8px 21px #293337"});
-  gsap.to(".blocks", {opacity: 1, delay: 0.5, stagger: 0.05});
-  gsap.to(".title", {duration: 1, height: "115px", delay: 2, ease: "power1.inOut"});
-  gsap.to(".title", {duration: 1.5, rotate: "90deg", right: "-160px", position: "fixed", delay: 3, ease: "power1.inOut"});
-  gsap.to("i", {duration: 1.5, rotate: "-90deg", delay: 3.5, ease: "bounce"});
-  gsap.to(".glassLeft", {duration: 4, x: "-100%", delay: 2, ease: "power1"});
-  gsap.to(".glassRight", {duration: 4, x: "100%", delay: 2, ease: "power1"});
-  gsap.to(".circle1, .circle2", {duration: 0.5, opacity: 0, delay: 2});
-  gsap.to(".glass", {duration: 0.5, display: "none", opacity: 0, delay: 6});
-  gsap.to(".about", {display: "block", delay: 4}); 
-  gsap.to(".projects", {display: "block", delay: 4});
-  gsap.to(".contact", {display: "flex", delay: 4});
+  gsap.to(".blocks", {opacity: 1, delay: 0.5, stagger: 0.02});
+  gsap.to(".title", {duration: 1, height: "115px", delay: 1, ease: "power1.inOut"});
+  gsap.to(".title", {duration: 1.5, rotate: "90deg", right: "-140px", boxShadow: "4px 4px 8px #0f1314", position: "fixed", delay: 2, ease: "power1.inOut"});
+  gsap.to("i", {duration: 1.5, rotate: "-90deg", delay: 2.5, ease: "bounce"});
+  gsap.to(".glassLeft", {duration: 4, x: "-100%", delay: 1, ease: "power1"});
+  gsap.to(".glassRight", {duration: 4, x: "100%", delay: 1, ease: "power1"});
+  gsap.to(".circle1, .circle2", {duration: 1, opacity: 0, delay: 1.3});
+  gsap.to(".glass", {duration: 0.5, display: "none", opacity: 0, delay: 5});
+  gsap.to(".about", {display: "block", delay: 3}); 
+  gsap.to(".projects", {display: "block", delay: 3});
+  gsap.to(".contact", {display: "flex", delay: 3});
 }
 
 
